e52c1a48a33d0ca2092dcc1c3420da5e825c5c07
Trilce <trilcegodoy11@gmail.com>
2025-05-18 23:03:59 -0300

    project created with its dependencies

---
+++ src/main/java/com/tracker/money_tracker/MoneyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tracker.money_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyTrackerApplication.class, args);
+	}
+
+}
